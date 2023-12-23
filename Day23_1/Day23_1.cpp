c4663f49951d478ff331df5b903cac2cafe8dcf5
Sik <martin_sik@centrum.cz>
2023-12-23 15:00:12 +0100

    Cleaned Day 23 solutions

--- Day23_1/Day23_1.cpp
+++ Day23_1/Day23_1.cpp
@@ -31,52 +31,44 @@ int main()
 	struct El {
 		P p;
 		int steps;
-		bool operator<(const El& el) const {
-			return steps > el.steps || (steps == el.steps && p.y > el.p.y);
-		}
 		P p0, p1;
-		//std::set<P> previous;
 
 	};
-	std::vector<std::vector<int>> steps(lines.size());
-	for (auto& s : steps) {
-		s.resize(lines[0].size(), -1);
-	}
-	std::priority_queue<El> queue;
-	queue.push(El{ P{0,1}, 0, P{-1,-1},P{-1,-1} });
-	while (!queue.empty()) {
-		El el = queue.top();
-		queue.pop();
-		if (
-			el.p.y < 0 || el.p.y >= lines.size() || el.p0 == el.p || el.p1 == el.p || lines[el.p.y][el.p.x] == '#' || steps[el.p.y][el.p.x] >= el.steps) {
+	const P end{ int(lines.size() - 1), int(lines[0].size() - 2) };
+	int steps = 0;
+	std::vector<El> stack;
+	stack.push_back(El{ P{0,1}, 0, P{-1,-1},P{-1,-1} });
+	while (!stack.empty()) {
+		El el = stack.back();
+		stack.pop_back();
+		if (el.p.y < 0 || el.p.y >= lines.size() || el.p0 == el.p || el.p1 == el.p || lines[el.p.y][el.p.x] == '#') {
 			continue;
 		}
-		steps[el.p.y][el.p.x] = el.steps;
-		++el.steps;
-		if (el.steps % 100 == 0) {
-			std::cout << el.steps << std::endl;
+		if (el.p == end) {
+			steps = std::max(el.steps, steps);
 		}
+		++el.steps;
 		el.p1 = el.p0;
 		switch (lines[el.p.y][el.p.x]) {
 		case '>':
-			queue.push(El{ P{el.p.y ,el.p.x + 1}, el.steps, el.p, el.p0 });
+			stack.push_back(El{ P{el.p.y ,el.p.x + 1}, el.steps, el.p, el.p0 });
 			break;
 		case '<':
-			queue.push(El{ P{el.p.y ,el.p.x - 1}, el.steps, el.p, el.p0 });
+			stack.push_back(El{ P{el.p.y ,el.p.x - 1}, el.steps, el.p, el.p0 });
 			break;
 		case 'v':
-			queue.push(El{ P{el.p.y + 1,el.p.x}, el.steps, el.p, el.p0 });
+			stack.push_back(El{ P{el.p.y + 1,el.p.x}, el.steps, el.p, el.p0 });
 			break;
 		case '^':
-			queue.push(El{ P{el.p.y - 1 ,el.p.x}, el.steps, el.p, el.p0 });
+			stack.push_back(El{ P{el.p.y - 1 ,el.p.x}, el.steps, el.p, el.p0 });
 			break;
 		case '.':
-			queue.push(El{ P{el.p.y ,el.p.x + 1}, el.steps, el.p, el.p0 });
-			queue.push(El{ P{el.p.y ,el.p.x - 1}, el.steps, el.p, el.p0 });
-			queue.push(El{ P{el.p.y + 1,el.p.x}, el.steps, el.p, el.p0 });
-			queue.push(El{ P{el.p.y - 1 ,el.p.x}, el.steps, el.p, el.p0 });
+			stack.push_back(El{ P{el.p.y ,el.p.x + 1}, el.steps, el.p, el.p0 });
+			stack.push_back(El{ P{el.p.y ,el.p.x - 1}, el.steps, el.p, el.p0 });
+			stack.push_back(El{ P{el.p.y + 1,el.p.x}, el.steps, el.p, el.p0 });
+			stack.push_back(El{ P{el.p.y - 1 ,el.p.x}, el.steps, el.p, el.p0 });
 			break;
 		}
 	}
-	std::cout << steps.back()[lines[0].size() - 2] << std::endl;
+	std::cout << steps << std::endl;
 }
--- Day23_2/Day23_2.cpp
+++ Day23_2/Day23_2.cpp
@@ -13,25 +13,22 @@ struct P {
 	bool operator==(const P& p) const {
 		return p.x == x && p.y == y;
 	}
-	bool operator<(const P& p) const {
-		return y < p.y || (y == p.y && x < p.x);
-	}
 };
 
 int main()
 {
 	std::ifstream stream("..\\input23.txt");
 	std::vector<std::string> lines;
-	std::vector<P> vertices;
-	vertices.push_back(P{ 0,1 });
 	std::string line;
 	while (std::getline(stream, line)) {
 		if (line.empty()) {
 			continue;
 		}
 		lines.push_back(line);
-		int i = 0;
 	}
+	std::vector<P> vertices;
+	vertices.push_back(P{ 0,1 });
+	lines[0][1] = '#';
 	for (int i = 1; i < lines.size() - 1; ++i) {
 		for (int j = 1; j < lines[0].size() - 1; ++j) {
 			if (lines[i][j] != '#') {
@@ -42,22 +39,19 @@ int main()
 				cnt += lines[i][j + 1] != '#';
 				if (cnt > 2) {
 					vertices.push_back(P{ i,j });
+					lines[i][j] = '#';
 				}
 			}
 		}
 	}
-
+	lines.back()[lines[0].size() - 2] = '#';
 	vertices.push_back(P{ int(lines.size() - 1),int(lines[0].size() - 2) });
+
 	struct Edge {
 		int steps;
 		int vertex;
 	};
 	std::vector<std::vector<Edge>> dist(vertices.size());
-	std::set<P> v;
-	for (int i = 0; i < vertices.size(); ++i) {
-		v.insert(vertices[i]);
-	}
-
 	for (int i = 0; i < vertices.size(); ++i) {
 		for (int j = i + 1; j < vertices.size(); ++j) {
 			struct El {
@@ -67,20 +61,15 @@ int main()
 			};
 			std::vector<El> stack;
 			int steps = -1;
-			std::set<P> points;
 			stack.push_back(El{ vertices[i], 0 });
 			const auto push_back = [&](const El& el, P prev) {
-				if (el.p.y < 0 || el.p.y >= lines.size() || el.p == prev || lines[el.p.y][el.p.x] == '#') {
-					return;
-				}
 				if (el.p == vertices[j]) {
 					steps = el.steps;
 					return;
 				}
-				if (!(el.p == vertices[i]) && v.find(el.p) != v.end()) {
+				if (el.p.y < 0 || el.p.y >= lines.size() || el.p == prev || lines[el.p.y][el.p.x] == '#') {
 					return;
 				}
-				
 				stack.push_back(el);
 			};
 			while (!stack.empty()) {
@@ -95,8 +84,6 @@ int main()
 
 			if (steps != -1) {
 				dist[i].push_back(Edge{ steps, j });
-				points.erase(vertices[i]);
-				points.insert(vertices[j]);
 				dist[j].push_back(Edge{ steps, i });
 			}
 		}
@@ -105,14 +92,11 @@ int main()
 	struct El {
 		int vertexIndex;
 		int steps;
-		bool operator<(const El& el) const {
-			return steps < el.steps;
-		}
 		uint64_t previous = 0;
 	};
 	int steps = 0;
-	std::priority_queue<El> queue;
-	queue.push(El{ 0, 0 });
+	std::vector<El> stack;
+	stack.push_back(El{ 0, 0 });
 	const auto push = [&](El&& el) {
 		if ((el.previous & (1ull << uint64_t(el.vertexIndex))) > 0) {
 			return;
@@ -121,12 +105,12 @@ int main()
 			steps = std::max(steps, el.steps);
 			return;
 		}
-		queue.push(el);
+		stack.push_back(el);
 	};
 
-	while (!queue.empty()) {
-		El el = queue.top();
-		queue.pop();
+	while (!stack.empty()) {
+		El el = stack.back();
+		stack.pop_back();
 		el.previous |= 1ull << uint64_t(el.vertexIndex);
 		for (Edge& e : dist[el.vertexIndex]) {
 			push(El{ e.vertex, el.steps + e.steps, el.previous });
